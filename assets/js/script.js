1f420bf4f4d63909ce9d441d5a7142bd2af49e22
rikilega <rosedoland33@yahoo.com>
2023-04-19 18:06:10 -0700

    deleted my test js file and made sure script file is running properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,19 @@ const savedSearches = JSON.parse(localStorage.getItem('savedSearches') || "[]")
 const historicalEventsElem = document.getElementById("historicalEvents")
 const harvardImgs = document.getElementById("harvardimgs")
 const imgElem = document.getElementById("imgElem")
+const artInfo = document.getElementById("artInfo")
 const imgTitleElem = document.getElementById("imgTitle")
 const previousBtn = document.getElementById("previous")
 const nextBtn = document.getElementById("next")
 const backBtn = document.getElementById("back")
 const artContainer = document.getElementById("pagination-container")
 const imgBtns = document.querySelector('imgBtn')
+const historyDropDown = document.getElementById('historyDropDown')
+const option = document.querySelector('.option')
 
+
+// change selectionTextElem to the choice
+// function for getting text data
 
 // add event listener for search option choice
 dropDown.addEventListener("click", (event) => {
@@ -62,10 +68,10 @@ form.addEventListener('submit', event => {
   event.preventDefault();
   const searchDate = searchText.value;
   console.log(typeof searchDate)
-  displaySearchHistoryBtn(searchDate)
+
   console.log(!isNaN(searchDate))
   console.log(selectionTextElem.textContent.trim())
-   displaySearchHistoryBtn(searchDate)
+
       
   
   // if, else-if, else
@@ -199,19 +205,223 @@ function renderAllEvents(allEventsData) {
   
 }
 
+//display results from year search to HTML set disply for other elements
 function getItToDom(eventHTML) {
-  scriptHTML.innerHTML = eventHTML 
-  }
+scriptHTML.innerHTML = eventHTML 
+}
+
+//scroll page up if displayed data is not in view
+function seeHTML(scriptHTML) {
+  scriptHTML.scrollIntoView({ behavior: 'smooth' })
+}
+
+
+//function to get historical data from Year search
+async function getKeyWordEvent(searchDate) {
+    const imgs = []
+    const totalPages = 6
+    currentPage = 1
+    console.log(currentPage)
+    const harvardURL = `https://api.harvardartmuseums.org/object?&apikey=28d8f398-d2ea-4c6c-bfe0-53c46eed6acb&hasimage=1&keyword=${searchDate}&q=NOT+image.description:null&page=`
+    console.log(harvardURL + currentPage)
+    console.log(totalPages)
+    const fetchedImgs = []
+    //parse images from the first 6 pages returned from 
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++ ) {
+        const waitForImages = fetch(harvardURL + currentPage) ;
+        fetchedImgs.push(waitForImages)
+    }
   
-  //scroll page up if displayed data is not in view
-  function seeHTML(scriptHTML) {
-    scriptHTML.scrollIntoView({ behavior: 'smooth' })
+    try {
+      const responses = await Promise.all(fetchedImgs)
+        
+      for (const response of responses) {
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+      
+        data.records.forEach(record => {
+            if (record.images && record.images[0] && record.images[0].baseimageurl && record.title) {
+                console.log(record.images[0].baseimageurl)
+                const imgURL = record.images[0].baseimageurl
+                const imgTitle = record.title
+                console.log(imgs)
+              
+              const artistName = record.people[0]?.displayname || '';
+              const birthDeathDate = record.people[0]?.displaydate || '';
+               const culture = record.people[0]?.culture || '';
+              const birthPlace = record.people[0]?.birthplace || '';
+              
+              let nameInput = '';
+              let birthDeath = '';
+              let cultureInput = '';
+              let birthPlaceInput = '';
+
+  if (artistName !== '') {
+    nameInput = "The name of the artist is " + artistName;
   }
 
+  if (birthDeathDate !== '') {
+    birthDeath = "and lived approximately " + birthDeathDate;
+  }
 
+  if (culture !== '') {
+    cultureInput = ". He was " + culture;
+  }
 
+  if (birthPlace !== '') {
+    birthPlaceInput = "from" + birthPlace;
+  }
+              
+  const description = record.description || '';
+              
+            
+          
+           imgs.push({
+            imgURL,
+            imgTitle,
+            nameInput,
+            birthDeath,
+            cultureInput,
+            birthPlaceInput,
+            description
+           
+          });
+          
+          } else if (!record.images || !record.images[0] || !record.images[0].baseimageurl) {
+              console.log('no baseimage url')
+          } 
+          })
+            } 
+            
+            }
+        } catch (error) {
+        console.error('Error fetching images:', error.message);
+      }
+  
+    try {
+        const lastPage = imgs.length
+        console.log(lastPage)
+        const pageSize = 1;
+        let currentPage = 1;
+        function renderImages() {
+            const birthDeath = imgs[currentPage-1].birthDeath || '';
+            const culture = imgs[currentPage-1].cultureInput || '';
+            const birthPlace = imgs[currentPage-1].birthPlaceInput || '';
+            const artistName = imgs[currentPage-1].nameInput || '';
+            const displayedImg = imgs[currentPage-1].imgURL;
+            const description = imgs[currentPage-1].description || '';
+            const imgElemHTML = `<img src="${displayedImg}">`;
+            const artInfoHTML = `<p>${artistName}  ${culture}  ${birthPlace}  ${birthDeath} ${description}</p>`
+            const displayedImgTitle = imgs[currentPage-1].imgTitle
+            console.log(displayedImg)
+            imgTitleElem.innerHTML = displayedImgTitle
+            imgElem.innerHTML = imgElemHTML
+            artInfo.innerHTML = artInfoHTML
+            console.log(artInfoHTML)
+  
+            previousBtn.disabled = currentPage === 1
+            nextBtn.disabled = currentPage === lastPage
+      }
+  
+    function showPreviousPage() {
+        if (currentPage > 1) {
+            currentPage--;
+            renderImages();
+        }
+    }
+  
+    previousBtn.addEventListener('click', showPreviousPage)
+    nextBtn.addEventListener('click', showNextPage)
+  
+    function showPreviousPage() {
+        console.log("clicked previous button")
+        if(currentPage > 1) {
+            currentPage--;
+            renderImages()
+        }
+    }
+  
+    function showNextPage() {
+        console.log("clicked next button")
+        if (currentPage < lastPage) {
+            currentPage++;
+            renderImages();
+        }
+    }
+    displayArtButtons()
+    
+  
+    } catch (error) {
+    console.error('Error rendering images', error.message)
+    }
+  
+    const myHeaders = new Headers();
+    myHeaders.append("X-Api-Key", "cVYAGyxVsjKIeUf3l0dufoGDRN5uh06eJhAPjFdL");
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    console.log("fetching url:", keyWordURL + searchDate)
+    try {
+        const response = await fetch(keyWordURL + searchDate, requestOptions);
+        if (!response.ok) {
+        throw new Error("response no good")
+        }
+    
+        const allEventsData = await response.json()
+    const eventHTML = renderAllEvents(allEventsData);
+    
+    function saveArtSearch(searchDate, eventHTML, imgs) {
+        const savedSearches = JSON.parse(localStorage.getItem('savedSearches') || "[]")
+        savedSearches.push({ search: searchDate, history: eventHTML, imgs })
+        localStorage.setItem('savedSearches', JSON.stringify(savedSearches));
+    }
+    // savedSearches.push({searchDate, selectionTextElem, eventHTML, artInfoHTML, imgElemHTML})
+  
+    renderImages()
+    getItToDom(eventHTML);
+    saveArtSearch( searchDate, eventHTML, imgs )
+    console.log("save art search called");
+    
+    historicalEventsElem.style.display = "block"
+    seeHTML(scriptHTML)
+    
+    
+    } catch (error) {
+    console.error("Error");
+    }
+  
+  }
+   
+  function displayArtButtons() {
 
+    artContainer.style.display = "inline" 
+    imgBtns.style.display = "inline"
+    
+}
 
+  backBtn.addEventListener("click", event => {
+    if (event.target = backBtn) {
+      console.log("clicked back button")
+      form.style.display = "block";
+      dropDown.style.display = "block";
+      backBtn.style.display = "none";
+      previousBtn.style.display = "none";
+      nextBtn.style.display = "none";
+      imgElem.style.display = "none";
+      imgTitle.style.display = "none";
+      scriptHTML.style.display = "none";
+      artInfo.style.display = "none";
+      historicalEventsElem.style.display = "none"
+      
+      
+     
+    } else{
+      console.log("couldn't go back to the home display settings")
+    }
+  })
 
 
 
