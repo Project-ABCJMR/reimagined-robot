23feef2e6fbfb97094561edb2c66e0f9dff8768f
rikilega <rosedoland33@yahoo.com>
2023-04-21 09:06:17 -0700

    updated keywordsearch and pastSearches as they weren't working as they should. added to keywordsearch to parse more information from the havard fetch and display, added some css and html elements and style for the new information displayed and to help with issues in functions with issues.

--- assets/js/keyWordSearchScript.js
+++ assets/js/keyWordSearchScript.js
@@ -1,6 +1,6 @@
 async function getKeyWordEvent(searchDate) {
 
-
+    
     const imgs = []
     const totalPages = 6
     currentPage = 1
@@ -22,74 +22,117 @@ async function getKeyWordEvent(searchDate) {
           const data = await response.json();
           console.log(data)
       
-        data.records.forEach(record => {
-            if (record.images && record.images[0] && record.images[0].baseimageurl && record.title ) {
-                console.log(record.images[0].baseimageurl)
-                const imgURL = record.images[0].baseimageurl
-                const imgTitle = record.title
-                imgs.push( {imgURL, imgTitle} )
-                console.log(imgs)
-  
-            }   else if (!record.images || !record.images[0] || !record.images[0].baseimageurl) {
-              console.log('no baseimage url')
-          } 
-          })
-            } 
-            
-            }
-        } catch (error) {
-        console.error('Error fetching images:', error.message);
-      }
-  
-    try {
-        const lastPage = imgs.length
-        console.log(lastPage)
-        const pageSize = 1;
-        let currentPage = 1;
-        function renderImages() {
-            const displayedImg = imgs[currentPage-1].imgURL;
-            const imgElemHTML = `<img src="${displayedImg}">`;
-            const displayedImgTitle = imgs[currentPage-1].imgTitle
-            imgTitleElem.innerHTML = displayedImgTitle
-            console.log(displayedImg)
-            imgElem.innerHTML = imgElemHTML
-            console.log(currentPage)
-  
-            previousBtn.disabled = currentPage === 1
-            nextBtn.disabled = currentPage === lastPage
-      }
-  
-    function showPreviousPage() {
-        if (currentPage > 1) {
-            currentPage--;
-            renderImages();
+
+    data.records.forEach(record => {
+        if (record.images && record.images[0] && record.images[0].baseimageurl && record.title) {
+            console.log(record.images[0].baseimageurl)
+            const imgURL = record.images[0].baseimageurl
+            const imgTitle = record.title
+            console.log(imgs)
+          
+          const artistName = record.people[0]?.displayname || '';
+          const birthDeathDate = record.people[0]?.displaydate || '';
+           const culture = record.people[0]?.culture || '';
+          const birthPlace = record.people[0]?.birthplace || '';
+          
+          let nameInput = '';
+          let birthDeath = '';
+          let cultureInput = '';
+          let birthPlaceInput = '';
+
+if (artistName !== '') {
+nameInput = "The name of the artist is " + artistName;
+}
+
+if (birthDeathDate !== '') {
+birthDeath = "and lived approximately " + birthDeathDate;
+}
+
+if (culture !== '') {
+cultureInput = ". He was " + culture;
+}
+
+if (birthPlace !== '') {
+birthPlaceInput = "from" + birthPlace;
+}
+          
+const description = record.description || '';
+          
+        
+      
+       imgs.push({
+        imgURL,
+        imgTitle,
+        nameInput,
+        birthDeath,
+        cultureInput,
+        birthPlaceInput,
+        description
+       
+      });
+      
+      } else if (!record.images || !record.images[0] || !record.images[0].baseimageurl) {
+          console.log('no baseimage url')
+      } 
+      })
+        } 
+        
         }
-    }
-  
-    previousBtn.addEventListener('click', showPreviousPage)
-    nextBtn.addEventListener('click', showNextPage)
-  
-    function showPreviousPage() {
-        console.log("clicked previous button")
-        if(currentPage > 1) {
-            currentPage--;
-            renderImages()
-        }
-    }
-  
-    function showNextPage() {
-        console.log("clicked next button")
-        if (currentPage < lastPage) {
-            currentPage++;
-            renderImages();
-        }
-    }
-  
-    renderImages()
-  
     } catch (error) {
-    console.error('Error rendering images', error.message)
+    console.error('Error fetching images:', error.message);
+  }
+
+try {
+    const lastPage = imgs.length
+    console.log(lastPage)
+    const pageSize = 1;
+    let currentPage = 1;
+    function renderImages() {
+        const birthDeath = imgs[currentPage-1].birthDeath || '';
+        const culture = imgs[currentPage-1].cultureInput || '';
+        const birthPlace = imgs[currentPage-1].birthPlaceInput || '';
+        const artistName = imgs[currentPage-1].nameInput || '';
+        const displayedImg = imgs[currentPage-1].imgURL;
+        const description = imgs[currentPage-1].description || '';
+        const imgElemHTML = `<img src="${displayedImg}">`;
+        const artInfoHTML = `<p>${artistName}  ${culture}  ${birthPlace}  ${birthDeath} ${description}</p>`
+        const displayedImgTitle = imgs[currentPage-1].imgTitle
+        console.log(displayedImg)
+        imgTitleElem.innerHTML = displayedImgTitle
+        imgElem.innerHTML = imgElemHTML
+        artInfo.innerHTML = artInfoHTML
+        console.log(artInfoHTML)
+
+        previousBtn.disabled = currentPage === 1
+        nextBtn.disabled = currentPage === lastPage
+  }
+
+
+
+previousBtn.addEventListener('click', showPreviousPage)
+nextBtn.addEventListener('click', showNextPage)
+
+function showPreviousPage() {
+    console.log("clicked previous button")
+    if(currentPage > 1) {
+        currentPage--;
+        renderImages()
     }
+}
+
+function showNextPage() {
+    console.log("clicked next button")
+    if (currentPage < lastPage) {
+        currentPage++;
+        renderImages();
+    }
+}
+
+
+
+} catch (error) {
+console.error('Error rendering images', error.message)
+}
   
     const myHeaders = new Headers();
     myHeaders.append("X-Api-Key", "cVYAGyxVsjKIeUf3l0dufoGDRN5uh06eJhAPjFdL");
@@ -102,19 +145,20 @@ async function getKeyWordEvent(searchDate) {
     try {
         const response = await fetch(keyWordURL + searchDate, requestOptions);
         if (!response.ok) {
+            console.log("fetching history data good line 106")
         throw new Error("response no good")
         }
     
         const allEventsData = await response.json()
     const eventHTML = renderAllEvents(allEventsData);
-    saveSearch(searchDate, selectionTextElem, eventHTML)
+    // saveSearch(searchDate, selectionTextElem, eventHTML)
   
-    
+    renderImages()
     getItToDom(eventHTML);
     
     console.log(result);
     
-    historicalEventsElem.style.display = "block"
+    // historicalEventsElem.style.display = "block"
     seeHTML(scriptHTML)
     
     
@@ -124,6 +168,6 @@ async function getKeyWordEvent(searchDate) {
   
   }
     // Make the getKeyWordEvent function accessible from other JS files
-    window.getKeyWordEvent = getKeyWordEvent;
+    // window.getKeyWordEvent = getKeyWordEvent;
   
   
